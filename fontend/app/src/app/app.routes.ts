1520c642855398dd572e57ffd108e4324d7802f8
JoeyEkapob <honhon16461@gmail.com>
2025-01-29 17:09:04 +0700

    day 2 68

--- fontend/app/src/app/app.routes.ts
+++ fontend/app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [{
+    path:'',
+    component: SigninComponent,
+},];
--- fontend/app/src/app/signin/signin.component.ts
+++ fontend/app/src/app/signin/signin.component.ts
@@ -1,11 +1,60 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-signin',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
 export class SigninComponent {
+  token: string = '';
+  username: string = '';
+  password: string = '';
 
-}
+  constructor (private http: HttpClient){}
+
+  signIn(){
+  /*   console.log('sdfsdf')
+    return */
+     if(this.username == ""|| this.password == ""){
+      Swal.fire({
+        title:'ตรวจสอบข้อมูล',
+        text: 'โปรดกรอก username หริอ password',
+        icon:'error'
+      })
+ 
+    }else{
+      const payload = {
+        username: this.username,
+        password: this.password
+      }
+      try{
+        this.http.post('http://localhost:3000/api/user/signin',payload).subscribe((res:any)=>{
+          this.token = res.token;
+          localStorage.setItem('angular_token',this.token)
+          localStorage.setItem('angular_username',this.username)
+     
+
+          location.reload()
+        },(err:any) => {
+          Swal.fire({
+            title:'ตรวจสอบข้อมูล',
+            text:'username invailid',
+            icon:'error'
+          })
+        })
+      }catch(e:any){
+          Swal.fire({
+            title:'error',
+            text:e.message,
+            icon:'error'
+          })
+        }
+      }
+    } 
+  }
+
